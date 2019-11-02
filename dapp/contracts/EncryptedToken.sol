14818338ebe75111ca96f730bc1fbfd81417055b
Outvoker <704457936@qq.com>
2019-11-02 16:36:01 +0800

    p73

--- dapp/contracts/EncryptedToken.sol
+++ dapp/contracts/EncryptedToken.sol
@@ -1,19 +1,19 @@
-pragma solidity >=0.4.17 <0.6.0;
+pragma solidity >=0.4.21 <0.6.0;
 
 contract EncryptedToken {
     uint256 INITIAL_SUPPLY = 666666;
     mapping(address => uint256) balances;
-    function EncryptedToken(){
+    constructor() public{
         balances[msg.sender] = INITIAL_SUPPLY;
     }
 
-    function transfer(address _to, uint256 _amount) {
-        assert(balances[msg.sender] < _amount);
+    function transfer(address _to, uint256 _amount) public {
+        assert(balances[msg.sender] >= _amount);
         balances[msg.sender] -= _amount;
-        balances[_to] += amount;
+        balances[_to] += _amount;
     }
 
-    function balanceOf(address _owner) constant returns {
+    function balanceOf(address _owner) public view returns (uint256) {
         return balances[_owner];
     }
 }
