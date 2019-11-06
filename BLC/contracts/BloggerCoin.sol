c8307574b505166147b0361f614b4845aafcc24f
Outvoker <704457936@qq.com>
2019-11-06 10:18:12 +0800

    11.6

---
+++ BLC/contracts/BloggerCoin.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.4.21 <0.6.0;
+import "../node_modules/zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+
+contract BloggerCoin is StandardToken {
+    string public name = "BloggerCoin";
+    string public symbol = "BLC";
+    uint8 public decimals = 4;
+    uint256 public INITIAL_SUPPLY = 666666;
+    constructor() {
+        totalSupply = INITIAL_SUPPLY;
+        balances[msg.sender] = INITIAL_SUPPLY;
+    }
+}
